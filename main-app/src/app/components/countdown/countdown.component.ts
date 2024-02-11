d754735a8520c1ad4c7d3caa1faefeb53cb18df2
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-02-11 05:29:52 -0500

    Add count down timers using signals

--- main-app/src/app/components/countdown/countdown.component.ts
+++ main-app/src/app/components/countdown/countdown.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  signal,
+  OnDestroy,
+  computed,
+  Inject,
+  PLATFORM_ID,
+} from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-countdown',
@@ -6,4 +15,40 @@ import { Component } from '@angular/core';
   styleUrl: './countdown.component.sass',
   standalone: true,
 })
-export class CountdownComponent {}
+export class CountdownComponent implements OnDestroy {
+  private _countdownDate: Date = new Date('2024-03-08T00:00:00.000Z');
+  private timeRemaining = signal(this.getTimeRemaining);
+  private readonly timer?: Subscription;
+  private isBrowser: boolean = isPlatformBrowser(this.platformId);
+
+  daysRemaining = computed(() =>
+    Math.floor(this.timeRemaining() / (1000 * 60 * 60 * 24)),
+  );
+  hoursRemaining = computed(() =>
+    Math.floor(
+      (this.timeRemaining() % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+    ),
+  );
+  minutesRemaining = computed(() =>
+    Math.floor((this.timeRemaining() % (1000 * 60 * 60)) / (1000 * 60)),
+  );
+  secondsRemaining = computed(() =>
+    Math.floor((this.timeRemaining() % (1000 * 60)) / 1000),
+  );
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {
+    if (this.isBrowser) {
+      this.timer = interval(1000).subscribe(() => {
+        this.timeRemaining.set(this.getTimeRemaining);
+      });
+    }
+  }
+  get getTimeRemaining(): number {
+    return this._countdownDate.getTime() - new Date().getTime();
+  }
+  ngOnDestroy() {
+    if (this.timer) {
+      this.timer.unsubscribe();
+    }
+  }
+}
