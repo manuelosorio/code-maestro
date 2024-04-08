4d9a151d7878c9b8cdaaacddc7c558ae176ddeb9
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-04-08 01:59:16 -0400

    Add unit test for nav component

--- main-app/src/app/components/nav/nav.component.spec.ts
+++ main-app/src/app/components/nav/nav.component.spec.ts
@@ -1,16 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { NavComponent } from './nav.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, AppComponent],
+      imports: [NavComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should create nav component', () => {
+    const fixture = TestBed.createComponent(NavComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
