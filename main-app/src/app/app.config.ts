c634073fb600814026588d1da6294a8390c8db6c
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-04-09 02:26:34 -0400

    Remove trpc
    Wasn't being used at all

--- main-app/src/app/app.config.ts
+++ main-app/src/app/app.config.ts
@@ -3,13 +3,10 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
 
-import { provideTrpcClient } from '../trpc-client';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideFileRouter(),
     provideClientHydration(),
-    provideHttpClient(withFetch()),
-    provideTrpcClient(),
+    provideHttpClient(withFetch())
   ],
 };
