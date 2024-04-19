6a361e1775fc517763632887bf32e0d97501f4d8
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-04-18 20:12:52 -0400

    Add http client module to app component

--- main-app/src/app/app.component.ts
+++ main-app/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'main-app-root',
   standalone: true,
   imports: [RouterOutlet, NavComponent],
+  providers: [HttpClientModule],
   template: `
     <app-nav></app-nav>
     <router-outlet></router-outlet>
