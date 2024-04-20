3416bd048c981c4c45bd40b9685160a2c8234026
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2024-04-20 01:40:21 -0400

    Update get auth api conditional

--- main-app/src/server/routes/v1/auth/index.get.ts
+++ main-app/src/server/routes/v1/auth/index.get.ts
@@ -1,21 +1,15 @@
-import { createError, defineEventHandler } from 'h3';
+import { defineEventHandler } from 'h3';
 
 export default defineEventHandler(async (event) => {
   const session = event.context['session'];
-  if (session) {
-    if (!session.user_id) {
-      return {
-        statusCode: 200,
-        authenticated: false,
-      };
-    }
+  if (session && session.user_id) {
     return {
       statusCode: 200,
       authenticated: true,
     };
   }
-  throw createError({
-    statusCode: 400,
-    message: 'Unknown error!',
-  });
+  return {
+    statusCode: 200,
+    authenticated: false,
+  };
 });
